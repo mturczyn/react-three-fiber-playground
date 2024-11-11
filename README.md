3b78feb9172f042be914047f0adc105433e55bbc
Michal Turczyn <turek1992@o2.pl>
2024-11-11 18:42:23 +0100

    Finished example from youtube. Added some notes.

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
+# React Three Fiber
+
+This application is used as a playground for React Three Fiber.
+
+Resources:
+
+-   [YouTube: How to Animate Object Motion in React Three Fiber](https://www.youtube.com/watch?v=cTTF0vnYxnw)
+-   [CodeSandBox from above video](How to Animate Object Motion in React Three Fiber)
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+-   [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+-   [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/YouTubeExample/YouTubeExample.jsx
+++ src/YouTubeExample/YouTubeExample.jsx
@@ -2,21 +2,70 @@ import { Cone, OrbitControls, Plane, Sphere } from '@react-three/drei'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useRef, useEffect } from 'react'
 import Instruments from './Instruments'
+import { Vector3 } from 'three'
+import { useInstrumentation } from './useInstrumentation'
+import './YouTubeExample.css'
+
+const FORCE_DAMPENING = 0.01
+const ACCEL_DAMPENING = 0.01
+const VELOCITY_DAMPENING = 0.01
+const MIN_ACCEL = 0
+const MAX_ACCEL = 1
+const MIN_SPEED = 10
+const MAX_SPEED = 30
+
+const min = new Vector3(4, 2, 2)
+const max = new Vector3(-4, -2, -1)
 
 const Mover = () => {
     const moverRef = useRef(null)
-    useFrame(() => {
-        if (!moverRef.current) return
+    const velocityRef = useRef(new Vector3(0, 0, 0))
+    const accelerationRef = useRef(new Vector3(0.01, 0, 0.05))
+    const reportPosition = useInstrumentation((state) => state.setPosition)
+    const reportVelocity = useInstrumentation((state) => state.setVelocity)
+    const reportDirection = useInstrumentation((state) => state.setDirection)
+    const reportAcceleration = useInstrumentation(
+        (state) => state.setAcceleration
+    )
+    const reportForce = useInstrumentation((state) => state.setForce)
 
-        const position = moverRef.current.position
+    useFrame(({ raycaster }) => {
+        const mover = moverRef.current
+        if (!mover) return
 
-        position.x += 0.001
-        position.y += 0.001
+        const velocity = velocityRef.current
+        if (!velocity) return
+
+        const position = mover.position
+        const acceleration = accelerationRef.current
+
+        const pointerWorldPosition = new Vector3()
+        raycaster.ray.at(5, pointerWorldPosition)
+        const force = pointerWorldPosition.sub(position)
+        reportForce(force)
+        acceleration
+            .add(force.clone().multiplyScalar(FORCE_DAMPENING))
+            .clampLength(MIN_ACCEL, MAX_ACCEL)
+        reportAcceleration(acceleration)
+        velocity
+            .add(acceleration.clone().multiplyScalar(ACCEL_DAMPENING))
+            .clampLength(MIN_SPEED, MAX_SPEED)
+        reportVelocity(velocity)
+        position
+            .add(velocity.clone().multiplyScalar(VELOCITY_DAMPENING))
+            .min(min)
+            .max(max)
+        reportPosition(position)
+
+        const direction = new Vector3()
+        direction.copy(velocity).normalize()
+        mover.quaternion.setFromUnitVectors(new Vector3(0, 1, 0), direction)
+        reportDirection(direction)
     })
 
     return (
         <group ref={moverRef}>
-            <Cone args={[1, 2, 100, 100]}>
+            <Cone args={[0.1, 0.2, 6]}>
                 <meshLambertMaterial color="blue" />
             </Cone>
         </group>
